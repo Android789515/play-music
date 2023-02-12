0ac55aea5abcf65b009e9376f9b0d8a6a89a7afb
Android789515 <derflug@mailfence.com>
2023-02-12 15:18:20 -0600

    feat(library-view): render the library view top bar

--- src/renderer/src/features/library-view/LibraryView.tsx
+++ src/renderer/src/features/library-view/LibraryView.tsx
@@ -1,6 +1,9 @@
+import { LibraryViewTopBar } from './components/library-view-top-bar';
+
 export const LibraryView = () => {
    return (
       <div>
+         <LibraryViewTopBar />
       </div>
    );
 };
