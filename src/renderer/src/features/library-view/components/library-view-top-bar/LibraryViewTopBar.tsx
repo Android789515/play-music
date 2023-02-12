ec363e5633c2549d75abeac722dc9cfe27805b8c
Android789515 <derflug@mailfence.com>
2023-02-12 15:18:09 -0600

    feat(library-view): render the library view headers

--- src/renderer/src/features/library-view/components/library-view-top-bar/LibraryViewTopBar.tsx
+++ src/renderer/src/features/library-view/components/library-view-top-bar/LibraryViewTopBar.tsx
@@ -1,9 +1,11 @@
 import styles from './LibraryViewTopBar.module.scss';
 
+import { LibraryViewHeaders } from '../../features/library-view-headers';
+
 export const LibraryViewTopBar = () => {
    return (
       <div className={styles.libraryViewTopBar}>
-
+         <LibraryViewHeaders />
       </div>
    );
 };
--- src/renderer/src/features/library-view/features/library-view-headers/LibraryViewHeaders.tsx
+++ src/renderer/src/features/library-view/features/library-view-headers/LibraryViewHeaders.tsx
@@ -1,8 +1,22 @@
 import styles from './LibraryViewHeaders.module.scss';
 
+import { LibraryViewHeader } from '../../components/library-view-header'
+
 export const LibraryViewHeaders = () => {
    return (
       <div className={styles.libraryViewHeaders}>
+         <LibraryViewHeader
+            headerText={'Title'}
+            isSorting
+         />
+
+         <LibraryViewHeader
+            headerText={'Artist'}
+         />
+
+         <LibraryViewHeader
+            headerText={'Duration'}
+         />
       </div>
    );
 };
