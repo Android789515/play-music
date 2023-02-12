e14ab5834b2035c744832d00336b6449cbeb8453
Android789515 <derflug@mailfence.com>
2023-02-12 14:50:10 -0600

    feat(library-view): create the library view headers container
    fixup! feat(library-view): create the library view headers container

---
+++ src/renderer/src/features/library-view/features/library-view-headers/LibraryViewHeaders.tsx
@@ -0,0 +1,8 @@
+import styles from './LibraryViewHeaders.module.scss';
+
+export const LibraryViewHeaders = () => {
+   return (
+      <div className={styles.libraryViewHeaders}>
+      </div>
+   );
+};
