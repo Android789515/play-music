afa04e41044aa6f7b672cb63deae596c11fe40db
Android789515 <derflug@mailfence.com>
2023-02-12 13:49:39 -0600

    feat(header): render the new tab button in the app header

--- src/renderer/src/components/app-header/AppHeader.tsx
+++ src/renderer/src/components/app-header/AppHeader.tsx
@@ -2,12 +2,15 @@ import styles from './AppHeader.module.scss';
 
 import { AppHeaderLayout } from './components'
 import { Tabs } from 'features/tabs';
+import { NewTabButton } from 'features/new-tab-button';
 
 export const AppHeader = () => {
    return (
       <header className={styles.appHeader}>
          <AppHeaderLayout>
             <Tabs />
+
+            <NewTabButton />
          </AppHeaderLayout>
       </header>
    );
