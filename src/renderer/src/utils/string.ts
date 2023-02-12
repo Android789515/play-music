4450b9c892476b131a1ad3dd3397d3c69f929b2f
Android789515 <derflug@mailfence.com>
2023-02-11 23:26:14 -0600

    feat(utils): create a util for capitalizing strings

---
+++ src/renderer/src/utils/string.ts
@@ -0,0 +1,6 @@
+export const capitalize = (string: string) => {
+   const capitalizedFirstLetter = string[0].toUpperCase();
+   const restOfString = string.slice(1);
+
+   return capitalizedFirstLetter + restOfString;
+};
