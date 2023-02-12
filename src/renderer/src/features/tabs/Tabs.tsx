b3c78627b2ffe9a8c0b57fc2401fc529300e9c86
Android789515 <derflug@mailfence.com>
2023-02-12 11:55:57 -0600

    refactor(tabs): render the tabs with a list component

--- src/renderer/src/features/tabs/Tabs.tsx
+++ src/renderer/src/features/tabs/Tabs.tsx
@@ -1,13 +1,14 @@
 import styles from './Tabs.module.scss';
 
+import { List } from 'components/list';
 import { Tab } from './components';
 
 export const Tabs = () => {
    return (
-      <ul className={styles.tabs}>
+      <List customStyles={styles.tabs}>
          <Tab currentTab tabText={'Library'} />
 
          <Tab tabText={'Classic Rock'} />
-      </ul>
+      </List>
    );
 };
