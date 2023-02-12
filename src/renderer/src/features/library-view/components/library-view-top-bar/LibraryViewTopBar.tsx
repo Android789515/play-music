b4e8eadb0713f7f5d2ded96004f4466d0c1841e3
Android789515 <derflug@mailfence.com>
2023-02-12 15:33:47 -0600

    feat(library-view): render a search bar in the library view top bar

--- src/renderer/src/features/library-view/components/library-view-top-bar/LibraryViewTopBar.tsx
+++ src/renderer/src/features/library-view/components/library-view-top-bar/LibraryViewTopBar.tsx
@@ -1,10 +1,13 @@
 import styles from './LibraryViewTopBar.module.scss';
 
+import { SearchBar } from 'components/search-bar';
 import { LibraryViewHeaders } from '../../features/library-view-headers';
 
 export const LibraryViewTopBar = () => {
    return (
       <div className={styles.libraryViewTopBar}>
+         <SearchBar />
+
          <LibraryViewHeaders />
       </div>
    );
