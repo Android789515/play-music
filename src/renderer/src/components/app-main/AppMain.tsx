eacd19896fae7a079f348ebf5e681b7529cb0d11
Android789515 <derflug@mailfence.com>
2023-02-12 15:18:46 -0600

    feat(app): render the library view

--- src/renderer/src/components/app-main/AppMain.tsx
+++ src/renderer/src/components/app-main/AppMain.tsx
@@ -1,12 +1,13 @@
 import styles from './AppMain.module.scss';
 
 import { AppMainLayout } from './components';
+import { LibraryView } from 'features/library-view'
 
 export const AppMain = () => {
    return (
       <main className={styles.appMain}>
          <AppMainLayout>
-
+            <LibraryView />
          </AppMainLayout>
       </main>
    );
