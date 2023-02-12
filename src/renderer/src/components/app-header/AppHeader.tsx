8c6c72555a9c9087a4f4ff48b35475896cf86482
Android789515 <derflug@mailfence.com>
2023-02-11 23:18:58 -0600

    feat(tabs): create the tabs component and render it

--- src/renderer/src/components/app-header/AppHeader.tsx
+++ src/renderer/src/components/app-header/AppHeader.tsx
@@ -1,12 +1,13 @@
 import styles from './AppHeader.module.scss';
 
 import { AppHeaderLayout } from './components'
+import { Tabs } from 'features/tabs';
 
 export const AppHeader = () => {
    return (
       <header className={styles.appHeader}>
          <AppHeaderLayout>
-
+            <Tabs />
          </AppHeaderLayout>
       </header>
    );
---
+++ src/renderer/src/features/tabs/Tabs.tsx
@@ -0,0 +1,8 @@
+import styles from './Tabs.module.scss';
+
+export const Tabs = () => {
+   return (
+      <ul className={styles.tabs}>
+      </ul>
+   );
+};
