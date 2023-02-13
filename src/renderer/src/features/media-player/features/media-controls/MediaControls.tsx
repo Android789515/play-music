c1e98edf4b9e1429e4e1c35f72289678fda6ab24
Android789515 <derflug@mailfence.com>
2023-02-12 20:02:42 -0600

    feat(media-player): render each media control button
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/features/media-player/features/media-controls/MediaControls.tsx
+++ src/renderer/src/features/media-player/features/media-controls/MediaControls.tsx
@@ -1,12 +1,42 @@
+import { VolumeStates } from './features/volume-button';
+
 import styles from './MediaControls.module.scss';
 
 import { MediaControlsLayout } from './components'
+import { MediaControlButton } from './components/media-control-button';
+import { VolumeButton } from './features/volume-button';
 
 export const MediaControls = () => {
    return (
       <div className={styles.mediaControls}>
          <MediaControlsLayout>
+            <MediaControlButton
+               name='Rewind'
+            />
 
+            <MediaControlButton
+               name='Pause'
+            />
+
+            <MediaControlButton
+               name='Stop'
+            />
+
+            <MediaControlButton
+               name='Fast Forward'
+            />
+
+            <VolumeButton
+               volumeState={VolumeStates.medium}
+            />
+
+            <MediaControlButton
+               name='Loop'
+            />
+
+            <MediaControlButton
+               name='Shuffle'
+            />
          </MediaControlsLayout>
       </div>
    );
