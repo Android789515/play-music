52fe954787c1ad614f1892a4bf0437a40ab8d187
Android789515 <derflug@mailfence.com>
2023-02-12 20:03:12 -0600

    feat(media-player): render the media player
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/components/app-footer/AppFooter.tsx
+++ src/renderer/src/components/app-footer/AppFooter.tsx
@@ -1,12 +1,13 @@
 import styles from './AppFooter.module.scss';
 
 import { AppFooterLayout } from './components';
+import { MediaPlayer } from 'features/media-player';
 
 export const AppFooter = () => {
    return (
       <footer className={styles.appFooter}>
          <AppFooterLayout>
-
+            <MediaPlayer />
          </AppFooterLayout>
       </footer>
    );
