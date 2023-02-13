10924d58be8351ba97b886db5c95be02854721ca
Android789515 <derflug@mailfence.com>
2023-02-12 20:03:00 -0600

    feat(media-player): render each media player component
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/features/media-player/MediaPlayer.tsx
+++ src/renderer/src/features/media-player/MediaPlayer.tsx
@@ -1,12 +1,23 @@
 import styles from './MediaPlayer.module.scss';
 
 import { MediaPlayerLayout } from './components';
+import { SongPlaying } from './features/song-playing';
+import { MediaControls } from './features/media-controls';
+import { SongDurationBar } from './features/media-controls/features/song-duration-bar';
 
 export const MediaPlayer = () => {
    return (
       <div className={styles.mediaPlayer}>
          <MediaPlayerLayout>
+            <SongPlaying
+               songTitle='Rondo Alla Turca'
+               songArtist='Mozart'
+               songDuration='3:20'
+            />
 
+            <MediaControls />
+
+            <SongDurationBar />
          </MediaPlayerLayout>
       </div>
    );
