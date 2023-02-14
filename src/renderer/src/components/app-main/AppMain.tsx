06b1e717ff70ca7c6c2910281f109e8de288359f
Android789515 <derflug@mailfence.com>
2023-02-14 17:58:55 -0600

    fix(library-view): fix library view top bar not sticking to the top
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/components/app-main/AppMain.tsx
+++ src/renderer/src/components/app-main/AppMain.tsx
@@ -2,15 +2,12 @@ import styles from './AppMain.module.scss';
 
 import { AppMainLayout } from './components';
 import { LibraryView } from 'features/library-view'
-import { Songs } from 'features/songs';
 
 export const AppMain = () => {
    return (
       <main className={styles.appMain}>
          <AppMainLayout>
             <LibraryView />
-
-            <Songs />
          </AppMainLayout>
       </main>
    );
--- src/renderer/src/features/library-view/LibraryView.tsx
+++ src/renderer/src/features/library-view/LibraryView.tsx
@@ -1,9 +1,12 @@
 import { LibraryViewTopBar } from './components/library-view-top-bar';
+import { Songs } from 'features/songs';
 
 export const LibraryView = () => {
    return (
       <div>
          <LibraryViewTopBar />
+
+         <Songs />
       </div>
    );
 };
