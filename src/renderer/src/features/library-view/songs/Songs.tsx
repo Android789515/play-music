3edc92f280ae0e42525aad19760f0f66313c3a77
Android789515 <derflug@mailfence.com>
2023-02-17 19:20:52 -0600

    refactor(songs): clear fake song data
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/features/library-view/songs/Songs.tsx
+++ src/renderer/src/features/library-view/songs/Songs.tsx
@@ -3,31 +3,35 @@ import { Song } from './components';
 import { SongInfo } from './components';
 
 export const Songs = () => {
-   const dummyData = [
-      { title: 'Rondo Alla Turca', artist: 'Mozart', duration: '3:20' },
-      { title: 'Fur Elise', artist: 'Beethoven', duration: '2:12' },
+   // const dummyData = [
+   //    { title: 'Rondo Alla Turca', artist: 'Mozart', duration: '3:20' },
+   //    { title: 'Fur Elise', artist: 'Beethoven', duration: '2:12' },
 
-      { title: `6th Symphony Orchestra 6th Symphony Orchestra6th
-                  Symphony Orchestra6th Symphony Orchestra6th
-                  Symphony Orchestra6th Symphony Orchestra6th
-                  Symphony Orchestra`, artist: 'Mozart', duration: '4:16' },
+   //    { title: `6th Symphony Orchestra 6th Symphony Orchestra6th
+   //                Symphony Orchestra6th Symphony Orchestra6th
+   //                Symphony Orchestra6th Symphony Orchestra6th
+   //                Symphony Orchestra`, artist: 'Mozart', duration: '4:16' },
 
-      { title: 'Nikijomi Sakiyatori', artist: 'Rin Hashitora', duration: '4:00' },
-      { title: 'Das Einzelzimmer', artist: 'Hoffmann Kreutz', duration: '3:45' },
-      { title: 'Le Meuljeko', artist: 'Artyk Marfyjlko', duration: '5:21' },
+   //    { title: 'Nikijomi Sakiyatori', artist: 'Rin Hashitora', duration: '4:00' },
+   //    { title: 'Das Einzelzimmer', artist: 'Hoffmann Kreutz', duration: '3:45' },
+   //    { title: 'Le Meuljeko', artist: 'Artyk Marfyjlko', duration: '5:21' },
 
-      { title: 'Great Class', artist: `Stone BlockStone BlockStone BlockStone BlockStone
-                  BlockStone BlockStone BlockStone BlockStone
-                  BlockStone BlockStone BlockStone BlockStone
-                  BlockStone BlockStone Block`, duration: '4:16' },
+   //    { title: 'Great Class', artist: `Stone BlockStone BlockStone BlockStone BlockStone
+   //                BlockStone BlockStone BlockStone BlockStone
+   //                BlockStone BlockStone BlockStone BlockStone
+   //                BlockStone BlockStone Block`, duration: '4:16' },
 
-      { title: 'Der Flug verspatet sich!', artist: 'Hoffmann Kreutz', duration: '2:18' },
-      { title: 'Don Rio', artist: 'Tejaka Moria', duration: '3:12' },
-      { title: 'Hat Song', artist: 'John Stewart', duration: '2:54' },
-      { title: 'Tokiyomi no Saori', artist: 'Rin Hashitora', duration: '4:36' },
-      { title: 'Gobble Gobble - Turkey Song', artist: 'Unknown', duration: '2:45' },
-      { title: 'Epitome of Hyberbole', artist: 'Standup Chair', duration: '35:23' },
-   ];
+   //    { title: 'Der Flug verspatet sich!', artist: 'Hoffmann Kreutz', duration: '2:18' },
+   //    { title: 'Don Rio', artist: 'Tejaka Moria', duration: '3:12' },
+   //    { title: 'Hat Song', artist: 'John Stewart', duration: '2:54' },
+   //    { title: 'Tokiyomi no Saori', artist: 'Rin Hashitora', duration: '4:36' },
+   //    { title: 'Gobble Gobble - Turkey Song', artist: 'Unknown', duration: '2:45' },
+   //    { title: 'Epitome of Hyberbole', artist: 'Standup Chair', duration: '35:23' },
+   // ];
+
+   // const dummyData = [];
+
+   const dummyData = [];
 
    const SongComponents = dummyData.map((song, index) => {
       const { title, artist, duration } = song;
