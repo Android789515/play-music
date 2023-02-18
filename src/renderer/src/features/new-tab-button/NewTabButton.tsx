f9c0a5f1944a9db909d112d496298f6f4030c434
Android789515 <derflug@mailfence.com>
2023-02-18 15:01:54 -0600

    feat(tabs): add basic functionality to the new tab button
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/features/new-tab-button/NewTabButton.tsx
+++ src/renderer/src/features/new-tab-button/NewTabButton.tsx
@@ -1,15 +1,29 @@
+import { v4 as newUUID } from 'uuid';
+
+import { useTabs } from 'features/tabs';
+
 import plusIcon from './assets/icons/plus.svg';
 import styles from './NewTabButton.module.scss';
 
 import { IconButton } from 'components/icon-button';
 
 export const NewTabButton = () => {
+   const { openTab, setCurrentTab } = useTabs();
+
+   const handleClick = () => {
+      const newTab = { id: newUUID(), name: 'New Tab' };
+      
+      openTab(newTab);
+      setCurrentTab(newTab);
+   };
+
    return (
       <IconButton
          name={'New Tab Button'}
          iconPath={plusIcon}
          buttonStyles={styles.newTabButton}
          iconStyles={styles.plusIcon}
+         onClick={handleClick}
       />
    );
 };
