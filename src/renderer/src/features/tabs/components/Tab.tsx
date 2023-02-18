a687502c99d9441c72dc29da6d5f15f060727c77
Android789515 <derflug@mailfence.com>
2023-02-18 14:14:18 -0600

    refactor(tabs): refactor the tab component to set the current tab
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/features/tabs/components/Tab.tsx
+++ src/renderer/src/features/tabs/components/Tab.tsx
@@ -1,22 +1,25 @@
-import { capitalize } from 'renderer/utils/string';
+import type { Tab as TabType } from '../types';
+import { capitalize } from 'utils/string';
 
 import styles from './Tab.module.scss';
 
 interface Props {
+   tab: TabType;
    currentTab?: boolean;
-   tabText: string;
+   setCurrentTab: (tab: TabType) => void; // Sets state
 }
 
-export const Tab = ({ currentTab, tabText }: Props) => {
+export const Tab = ({ tab, currentTab, setCurrentTab }: Props) => {
    return (
       <li
          className={`
-         ${styles.tab}
-         ${currentTab ? styles.currentTab : ''}
-      `}
+            ${styles.tab}
+            ${currentTab ? styles.currentTab : ''}
+         `}
+         onClick={() => setCurrentTab(tab)}
       >
          <h2 tabIndex={1} className={styles.tabText}>
-            {capitalize(tabText)}
+            {capitalize(tab.name)}
          </h2>
       </li>
    );
