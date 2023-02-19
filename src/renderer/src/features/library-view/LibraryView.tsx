1c71cabb98fb6d188ba6244c271510d3c0d2be06
Android789515 <derflug@mailfence.com>
2023-02-19 16:29:37 -0600

    feat(library-view): render songs from the current tab
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/features/library-view/LibraryView.tsx
+++ src/renderer/src/features/library-view/LibraryView.tsx
@@ -1,12 +1,26 @@
+import { useTabs } from 'features/tabs';
+
 import { LibraryViewTopBar } from './components/library-view-top-bar';
 import { Songs } from './songs';
 
 export const LibraryView = () => {
+   const { getTabs } = useTabs();
+
+   const getCurrentTabCollection = () => {
+      const [ currentTab ] = getTabs().filter(item => item.isCurrent);
+
+      // May be undefined
+      // Address by saving tab state
+      return currentTab?.collection || [];
+   };
+
    return (
       <div>
          <LibraryViewTopBar />
 
-         <Songs />
+         <Songs
+            songs={getCurrentTabCollection()}
+         />
       </div>
    );
 };
