640d162cb20b12bcf18e8e1b91507f069e450e75
Android789515 <derflug@mailfence.com>
2023-02-19 16:29:02 -0600

    feat(tabs): allow tabs to store collections to be rendered
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/features/tabs/stores/tabs.ts
+++ src/renderer/src/features/tabs/stores/tabs.ts
@@ -5,12 +5,39 @@ import type { Tab } from '../types';
 
 const namespace = '2aa78176-422b-4105-80c3-c39172040c3c';
 
+const dummyData = [
+   { title: 'Rondo Alla Turca', artist: 'Mozart', duration: '3:20' },
+   { title: 'Fur Elise', artist: 'Beethoven', duration: '2:12' },
+
+   { title: `6th Symphony Orchestra 6th Symphony Orchestra6th
+               Symphony Orchestra6th Symphony Orchestra6th
+               Symphony Orchestra6th Symphony Orchestra6th
+               Symphony Orchestra`, artist: 'Mozart', duration: '4:16' },
+
+   { title: 'Nikijomi Sakiyatori', artist: 'Rin Hashitora', duration: '4:00' },
+   { title: 'Das Einzelzimmer', artist: 'Hoffmann Kreutz', duration: '3:45' },
+   { title: 'Le Meuljeko', artist: 'Artyk Marfyjlko', duration: '5:21' },
+
+   { title: 'Great Class', artist: `Stone BlockStone BlockStone BlockStone BlockStone
+               BlockStone BlockStone BlockStone BlockStone
+               BlockStone BlockStone BlockStone BlockStone
+               BlockStone BlockStone Block`, duration: '4:16' },
+
+   { title: 'Der Flug verspatet sich!', artist: 'Hoffmann Kreutz', duration: '2:18' },
+   { title: 'Don Rio', artist: 'Tejaka Moria', duration: '3:12' },
+   { title: 'Hat Song', artist: 'John Stewart', duration: '2:54' },
+   { title: 'Tokiyomi no Saori', artist: 'Rin Hashitora', duration: '4:36' },
+   { title: 'Gobble Gobble - Turkey Song', artist: 'Unknown', duration: '2:45' },
+   { title: 'Epitome of Hyberbole', artist: 'Standup Chair', duration: '35:23' },
+];
+
 export const tabsState = atom<Set<Tab>>({
    key: newNameSpaceUUID('tabs', namespace),
    default: new Set([
       {
          id: newUUID(),
          name: 'Library',
+         collection: dummyData,
          isPermanent: true
       }
    ])
--- src/renderer/src/features/tabs/types/tabTypes.ts
+++ src/renderer/src/features/tabs/types/tabTypes.ts
@@ -1,8 +1,11 @@
 import type { UUID } from 'types/stringTypes';
 
+import type { Song } from 'features/library-view/songs';
+
 export interface Tab {
    id: UUID;
    name: string;
+   collection: Song[];
    isCurrent?: boolean;
    isPermanent?: boolean;
 }
