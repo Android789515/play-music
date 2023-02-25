d7bb9b23732c7a3ea57711c74d28e58bba608a90
Android789515 <derflug@mailfence.com>
2023-02-25 13:48:11 -0600

    feat(context-menu): refactor the menu items to render nested menus
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/components/context-menu/components/ContextMenuItem.tsx
+++ src/renderer/src/components/context-menu/components/ContextMenuItem.tsx
@@ -1,19 +1,49 @@
 import type { MouseEventHandler } from 'react';
 
+import type { Structure } from '../types';
+import type { ShowContextMenuHook } from '../api';
+import { useLogValueChange } from 'hooks/useLogValueChange';
+
 import styles from './ContextMenuItem.module.scss';
 
+import { ContextMenu } from '../ContextMenu';
+
 interface Props {
-   itemText: string;
-   action: MouseEventHandler;
+   contextMenuMethods: ShowContextMenuHook;
+   actionName: string;
+   action: MouseEventHandler | Structure;
 }
 
-export const ContextMenuItem = ({ itemText, action }: Props) => {
+export const ContextMenuItem = ({ contextMenuMethods, actionName, action }: Props) => {
+
+   const hasNestedMenu = typeof action === 'object';
+
+   const {
+      isContextMenuShown: isNestedMenuShown,
+      openContextMenu: openNestedMenu,
+      closeContextMenu: closeNestedMenu
+   } = contextMenuMethods;
+   
+   useLogValueChange(hasNestedMenu, { message: 'hasNestedMenu: ' + hasNestedMenu });
+   useLogValueChange(isNestedMenuShown(), { message: 'isNestedMenuShown(): ' + isNestedMenuShown() });
+   
    return (
       <li
          className={styles.contextMenuItem}
-         onClick={action}
+         onMouseEnter={hasNestedMenu ? openNestedMenu : closeNestedMenu}
+         onClick={hasNestedMenu ? openNestedMenu : action}
       >
-         {itemText}
+         <p className={styles.contextMenuActionName}>
+            {actionName}
+         </p>
+
+         { (hasNestedMenu && isNestedMenuShown()) ?
+            <ContextMenu
+               nested
+               structure={action}
+               closeContextMenu={() => {}}
+            />
+         : null}
       </li>
    );
 };
