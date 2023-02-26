305fcb569261d6854d937f152043ec99fa69eb85
Android789515 <derflug@mailfence.com>
2023-02-26 15:05:37 -0600

    feat(context-menu): create type validators for context menu items
    Signed-off-by: Android789515 <derflug@mailfence.com>

---
+++ src/renderer/src/components/context-menu/utils/typeValidators.ts
@@ -0,0 +1,17 @@
+import type {
+   ContextMenuItemWithMenu,
+   ContextMenuItemWithEvent,
+   OneOfAnyContextMenuItems,
+} from '../types';
+
+export const isContextMenuItemWithMenu = (
+   menuItem: OneOfAnyContextMenuItems
+): menuItem is ContextMenuItemWithMenu => {
+   return (menuItem as ContextMenuItemWithMenu).menu !== undefined;
+};
+
+export const isContextMenuItemWithEvent = (
+   menuItem: OneOfAnyContextMenuItems
+): menuItem is ContextMenuItemWithEvent => {
+   return (menuItem as ContextMenuItemWithEvent).onClick !== undefined;
+};
