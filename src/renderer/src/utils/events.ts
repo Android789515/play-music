99d9174b475ed17012ebcc07b82df357b011bdf9
Android789515 <derflug@mailfence.com>
2023-02-26 14:54:57 -0600

    feat(utils): create a util to select all the text in an element
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/utils/events.ts
+++ src/renderer/src/utils/events.ts
@@ -5,3 +5,15 @@ type Key = string;
 export const wereKeysPressed = ({ key: keyPressed }: KeyboardEvent, keys: Key[]) => {
    return keys.some(key => key === keyPressed);
 };
+
+export const selectAllText = (element: HTMLElement | null) => {
+   const selection = window.getSelection();
+   const range = document.createRange();
+
+   if (element) {
+      range.selectNodeContents(element);
+
+      selection?.removeAllRanges();
+      selection?.addRange(range);
+   }
+};
