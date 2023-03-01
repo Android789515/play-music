6b322ce53a0e9a196425a71dbba7746217a71dca
Android789515 <derflug@mailfence.com>
2023-02-28 18:10:36 -0600

    feat(utils): create number utils for formatting song time
    Signed-off-by: Android789515 <derflug@mailfence.com>

---
+++ src/utils/number.ts
@@ -0,0 +1,32 @@
+export const toDoubleDigit = (number: number) => {
+   const measurableNumber = String(number);
+
+   const isSingleDigit = measurableNumber.length === 1;
+   
+   const isMoreThanTwoDigits = measurableNumber.length > 2;
+
+   if (isSingleDigit) {
+      return '0' + number;
+   } else if (isMoreThanTwoDigits) {
+      return measurableNumber.slice(0, 2);
+   } else {
+      // Is number exactly two digits
+      return number;
+   }
+};
+
+export const formatSongTime = (timeInSeconds: number) => {
+   const minutes = toDoubleDigit(
+      Math.floor((timeInSeconds / 60) % 60)
+   );
+
+   const hours = toDoubleDigit(
+      Math.floor((timeInSeconds / 3600))
+   );
+
+   const leftOverSeconds = toDoubleDigit(
+      timeInSeconds % 3600
+   );
+
+   return `${hours}:${minutes}:${leftOverSeconds}`;
+};
