7871f93bf87147079e830789374c9c2ee456cfc2
Android789515 <derflug@mailfence.com>
2023-03-04 17:38:34 -0600

    feat(media-player): play the next song in the queue when available
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/features/media-player/MediaPlayer.tsx
+++ src/renderer/src/features/media-player/MediaPlayer.tsx
@@ -1,3 +1,9 @@
+import { useState, useCallback, useEffect } from 'react';
+
+import { isEmpty } from '@utils/array';
+import type { Song } from '@api/types';
+import { useSongQueue } from 'features/song-queue/api';
+
 import styles from './MediaPlayer.module.scss';
 
 import { MediaPlayerLayout } from './components';
@@ -5,10 +11,30 @@ import { MediaControls } from './features/media-controls';
 import { SongDurationBar } from './features/media-controls/features/song-duration-bar';
 
 export const MediaPlayer = () => {
+   const { getSongQueue, advanceSongQueue } = useSongQueue();
+
+   const [ songPlaying, setSongPlaying ] = useState<Song | null>(null);
+
+   const hasNextSong = !isEmpty(getSongQueue());
+
+   const playNextSong = () => {
+      setSongPlaying(advanceSongQueue());
+   };
+
+   useEffect(() => {
+      if (hasNextSong && !songPlaying) {
+         playNextSong();
+      }
+   }, [ hasNextSong, songPlaying, advanceSongQueue ]);
+
    return (
       <footer className={styles.mediaPlayer}>
          <MediaPlayerLayout>
-            <MediaControls />
+            <MediaControls
+               songPlaying={songPlaying}
+               setSongPlaying={setSongPlaying}
+               playNextSong={playNextSong}
+            />
 
             <SongDurationBar />
          </MediaPlayerLayout>
