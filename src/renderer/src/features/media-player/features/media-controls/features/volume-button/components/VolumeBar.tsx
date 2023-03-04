f79afa395732a7038706f2d502610a6aa51c4195
Android789515 <derflug@mailfence.com>
2023-03-04 17:41:22 -0600

    feat(media-player): set up volume bar dragging and changing
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/features/media-player/features/media-controls/features/volume-button/components/VolumeBar.tsx
+++ src/renderer/src/features/media-player/features/media-controls/features/volume-button/components/VolumeBar.tsx
@@ -1,20 +1,29 @@
+import type { Dispatch, SetStateAction } from 'react';
+
 import styles from './VolumeBar.module.scss';
 
 import { SlideableBar } from 'components/slideableBar';
 
 interface Props {
    increaseContrast?: boolean;
+   volume: number;
+   setVolume: Dispatch<SetStateAction<number>>;
 }
 
-export const VolumeBar = ({ increaseContrast: volumeButtonHover }: Props) => {
+export const VolumeBar = ({ increaseContrast: volumeButtonHover, volume, setVolume }: Props) => {
+
    return (
       <SlideableBar
-         value='45%'
+         value={`${volume * 100}%`}
+         setBarValue={barValue => setVolume(barValue / 100)}
          barStyles={`
             ${styles.volumeBar}
             ${volumeButtonHover ? styles.volumeBarHighContrast : ''}
          `}
-         barValueStyles={styles.volume}
+         barValueStyles={`
+            ${styles.volume}
+            ${volume === 1 ? styles.volumeFull : ''}
+         `}
       />
    );
 };
