11a6e632e15bc9d0a0cd4e85231558d20ff9ba47
Android789515 <derflug@mailfence.com>
2023-03-28 21:07:30 -0500

    feat(components): allow the slideable bar to be vertical
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/components/slideableBar/SlideableBar.tsx
+++ src/renderer/src/components/slideableBar/SlideableBar.tsx
@@ -7,30 +7,60 @@ import styles from './SlideableBar.module.scss';
 
 type BarValue = `${string}%`;
 interface Props {
+   vertical?: boolean;
    value: BarValue;
    setBarValue: (barValue: number) => void;
    barStyles?: CSS_Class;
    barValueStyles?: CSS_Class;
 }
 
-export const SlideableBar = ({ value, setBarValue, barStyles, barValueStyles }: Props) => {
+export const SlideableBar = ({ vertical, value, setBarValue, barStyles, barValueStyles }: Props) => {
    const { isDragging, enableDragging, disableDragging } = useDragBar();
 
-   const handleMouseMove = (event: MouseEvent) => {
-      const bar = event.target as HTMLDivElement;
+   const getBarSize = (bar: HTMLDivElement) => {
+      const { top, bottom, left, right } = bar.getBoundingClientRect();
 
-      const { left, right } = bar.getBoundingClientRect();
-      const barLeft = Math.floor(left);
-      const barRight = Math.floor(right);
+      return {
+         startOfBar: vertical ? bottom : left,
+         endOfBar: vertical ? top : right,
+      };
+   };
 
-      const barSize = barRight - barLeft;
-      const { clientX } = event;
+   const getBarValueClicked = (bar: HTMLDivElement, event: MouseEvent) => {
+      const { clientX, clientY } = event;
 
-      const locationClicked = clientX - barLeft;
+      const { startOfBar, endOfBar } = getBarSize(bar);
+      const barSize = endOfBar - startOfBar;
+
+      const locationClicked = (
+         vertical
+            ? clientY - startOfBar
+            : clientX - startOfBar
+      );
+      
       const percentage = (locationClicked / barSize) * 100;
 
-      const barValue = Number(percentage.toFixed(1));
-      setBarValue(barValue);
+      return Number(percentage.toFixed(1));
+   };
+
+   const validateBarValue = (barValue: number) => {
+      if (barValue < 0) {
+         return 0;
+      }
+
+      if (barValue > 100) {
+         return 100;
+      }
+
+      return barValue;
+   };
+
+   const handleMouseMove = (event: MouseEvent) => {
+      const bar = event.target as HTMLDivElement;      
+
+      const barValue = getBarValueClicked(bar, event);
+      
+      setBarValue(validateBarValue(barValue));
    };
 
    return (
@@ -39,6 +69,7 @@ export const SlideableBar = ({ value, setBarValue, barStyles, barValueStyles }: 
          className={`
             ${styles.bar}
             ${barStyles}
+            ${vertical ? styles.verticalBar : ''}
          `}
          onMouseDown={event => {
             enableDragging();
@@ -53,7 +84,11 @@ export const SlideableBar = ({ value, setBarValue, barStyles, barValueStyles }: 
          onMouseLeave={disableDragging}
       >
          <div
-            style={{ width: value }}
+            style={
+               vertical
+                  ? { height: value }
+                  : { width: value }
+            }
             className={`
                ${styles.barValue}
                ${value === '100%' ? styles.barValueFull : ''}
