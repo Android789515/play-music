1970051741494a3edd3117ae9d2309e1638723c8
Android789515 <derflug@mailfence.com>
2023-03-29 20:46:06 -0500

    feat(components): create a cover art component
    Signed-off-by: Android789515 <derflug@mailfence.com>

---
+++ src/renderer/src/components/cover-art/CoverArt.tsx
@@ -0,0 +1,16 @@
+import type { Path } from '@globalTypes/fileTypes';
+
+import defaultCoverArt from './assets/icons/default-cover-art.svg';
+
+interface Props {
+   coverArtLocation: Path;
+}
+
+export const CoverArt = ({ coverArtLocation }: Props) => {
+   return (
+      <img
+         src={coverArtLocation || defaultCoverArt}
+         alt='Cover art'
+      />
+   );
+};
