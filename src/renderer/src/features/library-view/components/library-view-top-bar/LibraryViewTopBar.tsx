9f4d5c8d49d3b550ff50bc78377380c224758cb2
Android789515 <derflug@mailfence.com>
2023-03-30 21:24:28 -0500

    refactor(library-view): remove the library view headers
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/features/library-view/components/library-view-top-bar/LibraryViewTopBar.tsx
+++ src/renderer/src/features/library-view/components/library-view-top-bar/LibraryViewTopBar.tsx
@@ -1,14 +1,11 @@
 import styles from './LibraryViewTopBar.module.scss';
 
 import { SearchBar } from 'components/search-bar';
-import { LibraryViewHeaders } from '../../features/library-view-headers';
 
 export const LibraryViewTopBar = () => {
    return (
       <div className={styles.libraryViewTopBar}>
          <SearchBar />
-
-         <LibraryViewHeaders />
       </div>
    );
 };
