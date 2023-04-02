b846c980261e0704b51f4c83e20f709160ce0201
Android789515 <derflug@mailfence.com>
2023-04-01 21:30:02 -0500

    feat(components): create basic cover art styles
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/components/cover-art/CoverArt.tsx
+++ src/renderer/src/components/cover-art/CoverArt.tsx
@@ -2,15 +2,20 @@ import type { Path } from '@globalTypes/fileTypes';
 
 import defaultCoverArt from './assets/icons/default-cover-art.svg';
 
+import styles from './CoverArt.module.scss';
+
 interface Props {
    coverArtLocation?: Path;
 }
 
 export const CoverArt = ({ coverArtLocation }: Props) => {
    return (
-      <img
-         src={coverArtLocation || defaultCoverArt}
-         alt='Cover art'
-      />
+      <div className={styles.coverArt}>
+         <img
+            src={coverArtLocation || defaultCoverArt}
+            alt='Cover art'
+            className={styles.coverArtIcon}
+         />
+      </div>
    );
 };
