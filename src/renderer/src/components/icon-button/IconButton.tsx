fec21fd119334eb3c860bd1ac330e31630ffd5a9
Android789515 <derflug@mailfence.com>
2023-07-03 18:02:45 -0500

    refactor(components): create icon button default styles to prevent overflow
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/components/icon-button/IconButton.tsx
+++ src/renderer/src/components/icon-button/IconButton.tsx
@@ -2,6 +2,8 @@ import type { HTMLAttributes, ReactNode } from 'react';
 
 import type { CSS_Class } from 'types/cssTypes';
 
+import defaultStyles from './IconButton.module.scss';
+
 import { Button } from 'components/button';
 
 interface Props extends HTMLAttributes<HTMLButtonElement> {
@@ -15,7 +17,10 @@ interface Props extends HTMLAttributes<HTMLButtonElement> {
 export const IconButton = ({ name, iconPath, buttonStyles = '', iconStyles, children, ...rest }: Props) => {
    return (
       <Button
-         customStyles={buttonStyles}
+         customStyles={`
+            ${defaultStyles.iconButton}
+            ${buttonStyles}
+         `}
          {...rest}
       >
          <img
