8a6ce7be1c6a45dc25f80903658b27ac401c7150
Android789515 <derflug@mailfence.com>
2023-07-03 14:37:11 -0500

    feat(song-queue): create a list component to show queued songs
    Signed-off-by: Android789515 <derflug@mailfence.com>

---
+++ src/renderer/src/features/song-queue/components/queued-songs/QueuedSongs.tsx
@@ -0,0 +1,32 @@
+import type { Song } from '@api/types';
+
+import styles from './QueuedSongs.module.scss';
+
+import { List } from 'components/list';
+import { SongInfo } from 'components/song-info';
+
+interface Props {
+   songQueue: Song[];
+}
+
+export const QueuedSongs = ({ songQueue }: Props) => {
+   return (
+      <List
+         customStyles={styles.queuedSongs}
+      >
+         {renderSongs(songQueue)}
+      </List>
+   );
+};
+
+const renderSongs = (songQueue: Song[]) => {
+   return songQueue.map(song => {
+      return (
+         <SongInfo
+            key={song.id}
+            songTitle={song.title}
+            songArtists={song.artists}
+         />
+      );
+   });
+};
