3bb2153020e01626261ba5a43de24cdb7c9fc1b3
Android789515 <derflug@mailfence.com>
2023-07-04 22:06:33 -0500

    refactor(components): render the reusable icon component in the search bar
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/components/search-bar/SearchBar.tsx
+++ src/renderer/src/components/search-bar/SearchBar.tsx
@@ -2,15 +2,16 @@ import searchIcon from './assets/icons/search.svg';
 import styles from './SearchBar.module.scss';
 
 import { SearchBarLayout } from './components';
+import { Icon } from 'components/icon';
 
 export const SearchBar = () => {
    return (
       <div className={styles.searchBar}>
          <SearchBarLayout>
-            <img
-               className={styles.searchIcon}
-               src={searchIcon}
-               alt="Search"
+            <Icon
+               iconPath={searchIcon}
+               alt={'Search'}
+               customStyles={styles.searchIcon}
             />
 
             <input
