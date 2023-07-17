3e320e949c855ef718e20326c4096b1f872222f6
Android789515 <derflug@mailfence.com>
2023-07-16 20:47:30 -0500

    refactor(library-view): replace function getting current tab collection
    Replace function that used to get the current tab collection with a
    simple call to get the current tab and a call to get its collection.
    
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/features/library-view/LibraryView.tsx
+++ src/renderer/src/features/library-view/LibraryView.tsx
@@ -3,20 +3,12 @@ import { useTabs } from 'features/tabs';
 import { SongCollection } from './components/song-collection';
 
 export const LibraryView = () => {
-   const { getTabs } = useTabs();
-
-   const getCurrentTabCollection = () => {
-      const [currentTab] = getTabs().filter(item => item.isCurrent);
-
-      // May be undefined
-      // Address by saving tab state
-      return currentTab?.collection || [];
-   };
+   const { getCurrentTab } = useTabs();
 
    return (
       <div>
          <SongCollection
-            songs={getCurrentTabCollection()}
+            songs={getCurrentTab()?.collection || []}
          />
       </div>
    );
