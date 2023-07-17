abd890439fff8380ce836e9b692a5f782c1befe2
Android789515 <derflug@mailfence.com>
2023-07-16 20:48:58 -0500

    feat(library-view): render the add stuff button in the library view
    Create a layout for the library view and render the add stuff button
    above the tab's collection.
    
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/features/library-view/LibraryView.tsx
+++ src/renderer/src/features/library-view/LibraryView.tsx
@@ -1,12 +1,20 @@
 import { useTabs } from 'features/tabs';
 
+import styles from './LibraryView.module.scss';
+
 import { SongCollection } from './components/song-collection';
+import { AddStuffButton } from './components/add-stuff-button';
 
 export const LibraryView = () => {
    const { getCurrentTab } = useTabs();
+   
 
    return (
-      <div>
+      <div
+         className={styles.libraryView}
+      >
+         <AddStuffButton />
+
          <SongCollection
             songs={getCurrentTab()?.collection || []}
          />
