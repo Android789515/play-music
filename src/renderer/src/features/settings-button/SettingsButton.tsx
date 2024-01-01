84bb3f9ebfcce697ed42ed9b99b4de85aa81d0a2
Android789515 <derflug@mailfence.com>
2024-01-01 16:36:46 -0600

    feat(settings): setup a settings dialog
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/features/settings-button/SettingsButton.tsx
+++ src/renderer/src/features/settings-button/SettingsButton.tsx
@@ -1,15 +1,29 @@
+import { useDialog } from 'components/dialog/api';
+
 import settingsIcon from './assets/icons/settings.svg';
 import styles from './SettingsButton.module.scss';
 
+import { SettingsDialog } from './components/settings-dialog';
 import { IconButton } from 'components/icon-button';
 
 export const SettingsButton = () => {
+   const { setDialogContent, openDialog } = useDialog();
+
+   const openSettingsDialog = () => {
+      setDialogContent({
+         content: <SettingsDialog />,
+      });
+
+      openDialog();
+   };
+
    return (
       <IconButton
          name={'Settings Button'}
          iconPath={settingsIcon}
          buttonStyles={styles.settingsButton}
          iconStyles={styles.settingsIcon}
+         onClick={openSettingsDialog}
       />
    );
 };
---
+++ src/renderer/src/features/settings-button/components/settings-dialog/SettingsDialog.tsx
@@ -0,0 +1,18 @@
+import type { Category } from '../categories/types';
+
+import styles from './SettingsDialog.module.scss';
+
+import { Categories } from '../categories';
+
+export const SettingsDialog = () => {
+   return (
+      <section
+         className={styles.dialogContent}
+      >
+
+         <section>
+            
+         </section>
+      </section>
+   );
+};
