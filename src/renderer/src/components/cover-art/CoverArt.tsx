5a18d0086c5ef759107dd178268f0ec8c2d80dbf
Android789515 <derflug@mailfence.com>
2024-01-01 20:30:52 -0600

    refactor(components): adjust the component styling for when cover art is present
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/components/cover-art/CoverArt.tsx
+++ src/renderer/src/components/cover-art/CoverArt.tsx
@@ -8,20 +8,28 @@ import styles from './CoverArt.module.scss';
 import { Icon } from 'components/icon';
 
 interface Props {
-   coverArtLocation?: Path;
+   coverArtLocation?: Path | null;
    additionalIconStyles?: CSS_Class;
 }
 
 export const CoverArt = ({ coverArtLocation, additionalIconStyles }: Props) => {
    return (
       <div
-         className={styles.coverArt}
+         className={
+            coverArtLocation
+               ? styles.coverArt
+               : styles.defaultCoverArt
+         }
       >
          <Icon
             iconPath={coverArtLocation || defaultCoverArt}
             alt={'Cover Art'}
             customStyles={`
-               ${styles.coverArtIcon}
+               ${
+                  coverArtLocation
+                     ? styles.coverArtIcon
+                     : styles.defaultCoverArtIcon
+               }
                ${additionalIconStyles}
             `}
          />
