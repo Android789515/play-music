983cbb3c2a6cb5ecd4a34340a95388157c884c2d
Android789515 <derflug@mailfence.com>
2024-01-20 17:53:20 -0600

    feat(settings): render the about section versions in a list
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/features/settings-button/components/about-section/AboutSection.tsx
+++ src/renderer/src/features/settings-button/components/about-section/AboutSection.tsx
@@ -1,12 +1,31 @@
 import styles from './AboutSection.module.scss';
 
+import { List } from 'components/list';
+
 export const AboutSection = () => {
    const Versions = Object.entries(window.appInfo.versions)
       .map(([ techName, versionNumber ], index) => {
          return (
-            <p key={index}>
-               {techName}: {versionNumber}
-            </p>
+            <li
+               key={index}
+               className={styles.version}
+            >
+               <span
+                  className={styles.techName}
+               >
+                  {techName}
+               </span>
+
+               <span>
+                  {': '}
+               </span>
+
+               <span
+                  className={styles.versionNumber}
+               >
+                  {versionNumber}
+               </span>
+            </li>
          );
       });
 
@@ -18,7 +37,11 @@ export const AboutSection = () => {
             About
          </h2>
 
-         {Versions}
+         <List
+            customStyles={styles.versions}
+         >
+            {Versions}
+         </List>
       </section>
    );
 };
