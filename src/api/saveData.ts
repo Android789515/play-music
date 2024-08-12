14d858061b00458d8c1e3646bf7b5d6061d2e089
Android789515 <derflug@mailfence.com>
2024-08-12 22:12:21 +0000

    feat(utils): centralize saved data keys
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/api/saveData.ts
+++ src/api/saveData.ts
@@ -1,15 +1,36 @@
 import { app } from 'electron';
-import { writeFile, existsSync, readFileSync } from 'fs';
+import { writeFile, writeFileSync, existsSync, readFileSync } from 'fs';
 import { join } from 'path';
 
 import type { UUID } from '../types/stringTypes';
 
 type StorageKey = UUID;
 
+const logDataKey = (key: StorageKey) => {
+   const keysFile = join(app.getPath('userData'), '.dataKeys');
+
+   if (existsSync(keysFile)) {
+      const keysFileContent = readFileSync(keysFile).toString();
+
+      const isNewKey = !keysFileContent.includes(key);
+
+      const maybeKey = isNewKey
+         ? `${keysFileContent}\n${key}`
+         : keysFileContent;
+
+      writeFileSync(keysFile, maybeKey);
+   } else {
+      writeFileSync(keysFile, key);
+   }
+
+};
+
 export const saveDataAPI = {
    saveData: {
       name: 'saveData',
       fn: (key: StorageKey, data: string) => {
+         logDataKey(key);
+
          const dataToSave = join(
             app.getPath('userData'),
             key,
