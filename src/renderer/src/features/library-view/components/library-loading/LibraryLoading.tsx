23ba6f5dc9e2fb6e5f969cb6446fd0e8c3e4d375
Android789515 <derflug@mailfence.com>
2024-08-17 23:44:19 +0000

    feat(library): add title to the loading component
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/features/library-view/components/library-loading/LibraryLoading.tsx
+++ src/renderer/src/features/library-view/components/library-loading/LibraryLoading.tsx
@@ -4,11 +4,19 @@ import { AsyncSpinner } from 'components/async-spinner';
 
 export const LibraryLoading = () => {
    return (
-      <AsyncSpinner
-         customStyles={{
-            layout: styles.loadingSpinnerLayout,
-            spinner: styles.loadingSpinner,
-         }}
-      />
+      <>
+         <h1
+            className={styles.loadingTitle}
+         >
+            Initializing Library...
+         </h1>
+
+         <AsyncSpinner
+            customStyles={{
+               layout: styles.loadingSpinnerLayout,
+               spinner: styles.loadingSpinner,
+            }}
+         />
+      </>
    );
 };
