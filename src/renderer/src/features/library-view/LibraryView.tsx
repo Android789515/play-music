ef795b9f14e6f26e9e15e5aa4481d3e01acfdfb6
Android789515 <derflug@mailfence.com>
2024-08-25 22:13:09 +0000

    style(library): disable eslint warning
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/features/library-view/LibraryView.tsx
+++ src/renderer/src/features/library-view/LibraryView.tsx
@@ -32,6 +32,7 @@ export const LibraryView = () => {
       elapseMinLoadingTime();
 
       loadLibrary();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
    }, []);
 
    const currentTab = getCurrentTab();
