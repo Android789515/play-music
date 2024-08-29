a028d4ce094bec9c2499f3b7559c085b7c5a60c9
Android789515 <derflug@mailfence.com>
2024-08-30 08:12:34 +0900

    feat(components): allow the list to accept a ref
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/components/list/List.tsx
+++ src/renderer/src/components/list/List.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from 'react';
+import type { ReactNode, MutableRefObject } from 'react';
 
 import type { CSS_Class } from 'types/cssTypes';
 
@@ -6,15 +6,19 @@ import styles from './List.module.scss';
 
 interface Props {
    customStyles?: CSS_Class;
+   needsRef?: MutableRefObject<HTMLUListElement | null>
    children: ReactNode;
 }
 
-export const List = ({ customStyles = '', children }: Props) => {
+export const List = ({ customStyles = '', needsRef, children }: Props) => {
    return (
-      <ul className={`
-         ${styles.listDefaults}
-         ${customStyles}
-      `}>
+      <ul
+         className={`
+            ${styles.listDefaults}
+            ${customStyles}
+         `}
+         ref={needsRef}
+      >
          {children}
       </ul>
    );
