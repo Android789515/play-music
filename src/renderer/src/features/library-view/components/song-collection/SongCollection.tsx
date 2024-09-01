b9b26ae29c791a0befd3b636b547f6f38f96c845
Android789515 <derflug@mailfence.com>
2024-09-02 07:17:11 +0900

    fix(library): hide missing songs
    Filter the incoming songs from the playlist to hide any songs that were
    moved or deleted.
    
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/renderer/src/features/library-view/components/song-collection/SongCollection.tsx
+++ src/renderer/src/features/library-view/components/song-collection/SongCollection.tsx
@@ -1,3 +1,5 @@
+import { type ReactNode, useState, useEffect } from 'react';
+
 import type { Song as SongType } from '@api/types';
 
 import { List } from 'components/list';
@@ -10,13 +12,36 @@ interface Props {
 }
 
 export const SongCollection = ({ songs }: Props) => {
-   const SongComponents = songs.map((song, index) => {
-      return (
-         <li key={index} className={styles.song}>
-            <SongButton song={song} />
-         </li>
-      );
-   });
+   const [ SongComponents, setSongComponents ] = useState<ReactNode>([]);
+
+   const createSongComponents = async () => {
+      const RefreshedSongs = songs.map(async (song, index) => {
+         const doesSongStillExist = await window.api.stillExists(song.path);
+
+         if (doesSongStillExist) {
+            return (
+               <li key={index} className={styles.song}>
+                  <SongButton song={song} />
+               </li>
+            );
+         } else {
+            return null;
+         }
+      });
+
+      setSongComponents(await Promise.all(RefreshedSongs));
+   };
+
+   const onCleanup = () => {
+      setSongComponents([]);
+   };
+
+   useEffect(() => {
+      createSongComponents();
+
+      return onCleanup;
+   // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [ songs ]);
 
    return (
       <List>
